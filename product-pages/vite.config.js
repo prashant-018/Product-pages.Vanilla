a4133263ec6176426caf0740c457e9348f304694
Prashant Shrivastava <tera-real-email@example.com>
2025-05-12 21:41:23 +0530

    four

--- product-pages/vite.config.js
+++ product-pages/vite.config.js
@@ -1,6 +1,5 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/product-pages/', // ← repo name ke hisaab se update karo
+  base: '/Product-pages.Vanilla/',
 });
